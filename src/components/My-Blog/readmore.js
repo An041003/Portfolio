1085155bc6d582af764cec7525f9139907449bef
An041003 <binhan214981@gmail.com>
2024-08-06 17:05:21 +0700

    Update readmore.js

--- src/components/My-Blog/readmore.js
+++ src/components/My-Blog/readmore.js
@@ -3,32 +3,34 @@ import './myblog.css';
 
 const ReadMore = ({ children }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isOverflowing, setIsOverflowing] = useState(false);
   const textRef = useRef(null); 
 
-useEffect(() => {
-  const textElement = textRef.current;
-  if (textElement) {
-    let lineHeight = window.getComputedStyle(textElement).lineHeight;
-    if (lineHeight === "normal") {
-      lineHeight = 24; 
-    } else {
-      lineHeight = parseInt(lineHeight);
-    }
-
-    if (!isNaN(lineHeight)) {
-      const lines = Math.ceil(textElement.scrollHeight / lineHeight);
-      console.log('Giá trị lines:', lines);
-      console.log('Scroll height:', textElement.scrollHeight);
-      if (lines > 4) { 
-        setIsExpanded(false); 
+  useEffect(() => {
+    const textElement = textRef.current;
+    if (textElement) {
+      let lineHeight = window.getComputedStyle(textElement).lineHeight;
+      if (lineHeight === "normal") {
+        lineHeight = 24;
+      } else {
+        lineHeight = parseInt(lineHeight);
       }
-    } else {
-      console.error('Không thể xác định lineHeight.');
-    }
-  }
-}, []);
 
-      
+      if (!isNaN(lineHeight)) {
+        const lines = Math.ceil(textElement.scrollHeight / lineHeight);
+        console.log('Giá trị lines:', lines);
+        console.log('Scroll height:', textElement.scrollHeight);
+        if (lines > 4) { 
+          setIsExpanded(false); 
+          setIsOverflowing(true);
+        } else {
+          setIsOverflowing(false);
+        }
+      } else {
+        console.error('Không thể xác định lineHeight.');
+      }
+    }
+  }, []);
 
   const toggleReadMore = () => {
     setIsExpanded(!isExpanded);
@@ -39,7 +41,7 @@ useEffect(() => {
       <p ref={textRef} className={isExpanded ? 'text expanded' : 'text'}>
         {children}
       </p>
-      {textRef.current && textRef.current.scrollHeight > 100 && ( 
+      {isOverflowing && ( 
         <span className="read-more-link" onClick={toggleReadMore}>
           {isExpanded ? 'Thu gọn' : 'Xem thêm'}
         </span>
